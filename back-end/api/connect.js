696af3e6432678b80ea2307deb4965fe89ee501c
João Pedro E. Silveira <108689656+jpsilveira11@users.noreply.github.com>
2025-06-06 22:30:21 -0300

    Update connect.js

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -4,11 +4,11 @@
 import { MongoClient } from "mongodb";
 
 const URI =
-  "mongodb+srv://fullstackjornada:qojI71xVU2aV8UKC@cluster0.v1qra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://jp:11jps2802@cluster0.krlxyrw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(URI);
 
-export const db = client.db("spotifyAula");
+export const db = client.db("Reactify");
 // const songCollection = await db.collection("songs").find({}).toArray();
 
 // console.log(songCollection);
